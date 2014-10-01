4cbc1629e04b96713a288bbea34b8592ccc7f826
Manuel Alejandro de Brito Fontes <aledbf@gmail.com>
2014-10-01 10:43:43 -0300

    feat(router): add firewall to the router.

---
+++ router/parent/README.md
@@ -0,0 +1,21 @@
+[**NAXSI**](https://github.com/nbs-system/naxsi) is an open-source, high performance, low rules maintenance WAF for NGINX.
+
+Why a firewall for nginx in deis?
+Well in the last weeks [Shellshock](https://shellshocker.net) showed a vulnerability that some apps (mostly CGI's) inside a web server can be exploited like is explained here [Inside Shellshock: How hackers are using it to exploit systems](https://blog.cloudflare.com/inside-shellshock)
+
+To reduce the contact surface of this attack and others (like sql injection and cross site scripting) this module is enabled by default.
+
+Example:
+```console
+TODO
+```
+
+The rules included are taken from this project [doxi-rules](https://bitbucket.org/lazy_dogtown/doxi-rules)
+
+Only this modules are enabled:
+
+|File|
+|web_app.rules       |detect exploit/misuse-attempts againts web-applications
+|web_server.rules    |generic rules to protect a webserver from misconfiguration and known mistakes / exploit-vectors 
+|active-mode.rules   |rules to configure active-mode (block)
+|naxsi_core          |core naxsi rules
---
+++ router/parent/build.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+
+export VERSION_NGINX=nginx-1.6.2
+export VERSION_TCP_PROXY=0.4.5
+export VERSION_NAXSI=0.53-2
+
+export BUILD_PATH=/tmp/build
+
+# nginx installation directory
+export PREFIX=/nginx
+
+rm -rf $PREFIX
+mkdir $PREFIX
+
+mkdir $BUILD_PATH
+cd $BUILD_PATH
+
+# install required packages to build
+apt-get update \
+  && apt-get install -y patch curl build-essential \
+  libpcre3 libpcre3-dev libssl-dev libgeoip-dev zlib1g-dev
+
+# grab the source files
+curl -sSL http://nginx.org/download/$VERSION_NGINX.tar.gz -o $BUILD_PATH/$VERSION_NGINX.tar.gz
+curl -sSL https://github.com/yaoweibin/nginx_tcp_proxy_module/archive/v$VERSION_TCP_PROXY.tar.gz -o $BUILD_PATH/$VERSION_TCP_PROXY.tar.gz
+curl -sSL https://github.com/nbs-system/naxsi/archive/$VERSION_NAXSI.tar.gz -o $BUILD_PATH/$VERSION_NAXSI.tar.gz
+
+# expand the source files
+tar xzf $VERSION_NGINX.tar.gz
+tar xzf $VERSION_TCP_PROXY.tar.gz
+tar xzf $VERSION_NAXSI.tar.gz
+
+# build nginx
+cd $BUILD_PATH/$VERSION_NGINX
+
+patch -p1 < $BUILD_PATH/nginx_tcp_proxy_module-$VERSION_TCP_PROXY/tcp.patch
+
+./configure \
+  --prefix=$PREFIX \
+  --pid-path=/run/nginx.pid \
+  --with-debug \
+  --with-pcre-jit \
+  --with-ipv6 \
+  --with-http_ssl_module \
+  --with-http_stub_status_module \
+  --with-http_realip_module \
+  --with-http_auth_request_module \
+  --with-http_addition_module \
+  --with-http_dav_module \
+  --with-http_geoip_module \
+  --with-http_gzip_static_module \
+  --with-http_spdy_module \
+  --with-http_sub_module \
+  --with-mail \
+  --with-mail_ssl_module \
+  --add-module=$BUILD_PATH/nginx_tcp_proxy_module-$VERSION_TCP_PROXY \
+  --add-module=$BUILD_PATH/naxsi-$VERSION_NAXSI/naxsi_src \
+  && make && make install
+
+mv /tmp/firewall /nginx/firewall
